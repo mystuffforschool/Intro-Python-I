9626d56ff43913ad17af3bba64e01802ce03bff9
Skyler Dowdy <skylerwebdev@gmail.com>
2020-03-09 18:17:15 -0400

    done

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x,y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -13,6 +15,11 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    res = 0
+    for arg in args:
+        res += arg
+    return res
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,6 +29,11 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
+def f2(x):
+    res = 0
+    for i in x:
+        res += i
+    return res
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -30,7 +42,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(x, d=1):
+    return x+d
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -42,8 +55,9 @@ print(f3(8))     # Should print 9
 # key: baz, value: 12
 #
 # Note: Google "python keyword arguments".
-
-# YOUR CODE HERE
+def f4(**kwords):
+  for key in kwords:
+    print(f'key: {key}, value: {kwords[key]}')
 
 # Should print
 # key: a, value: 12
@@ -62,4 +76,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
